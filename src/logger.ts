90a75aa16aaa41c4cc0f52d56a4a23d0ec475854
roblou <roblourens@gmail.com>
2016-10-05 11:40:52 -0700

    Add timestamp to log

--- src/logger.ts
+++ src/logger.ts
@@ -61,6 +61,10 @@ export function init(logCallback: ILogCallback, logFilePath?: string, logToConso
     _logger = new Logger(logCallback, logFilePath, logToConsole);
     if (logFilePath) {
         log(`Verbose logs are written to ${logFilePath}`);
+
+        const d = new Date();
+        const timestamp = d.toLocaleTimeString() + ', ' + d.toLocaleDateString();
+        log(timestamp);
     }
 }
 
