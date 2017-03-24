03ff157216633c220e9fad3e2d79111466f4ee3b
Rob Lourens <roblourens@gmail.com>
2017-03-24 14:25:31 -0700

    'data' now in OutputEvent constructor

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -8,9 +8,7 @@ import {OutputEvent} from 'vscode-debugadapter';
 let sendEvent: (event: DebugProtocol.Event) => void;
 
 export function reportEvent(name: string, data?: any): void {
-    // https://github.com/Microsoft/vscode-debugadapter-node/pull/77
-    const event = new OutputEvent(name, 'telemetry');
-    (<DebugProtocol.OutputEvent>event).body.data = data;
+    const event = new OutputEvent(name, 'telemetry', data);
 
     if (sendEvent) sendEvent(event);
 }
