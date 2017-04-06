a079490ea326988f0d72bc4ad4aac5d1b5323101
Rob Lourens <roblourens@gmail.com>
2017-04-05 21:46:26 -0700

    Unused import

--- src/chrome/consoleHelper.ts
+++ src/chrome/consoleHelper.ts
@@ -2,8 +2,6 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/
 
-import * as url from 'url';
-
 import Crdp from '../../crdp/crdp';
 import * as ChromeUtils from './chromeUtils';
 
