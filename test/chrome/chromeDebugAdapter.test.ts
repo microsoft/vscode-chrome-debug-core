1317dc6f84dbdf5094477c077b88e06837c416a6
Rob Lourens <roblourens@gmail.com>
2017-08-17 16:37:19 -0700

    Fix tests for includeCommandLineAPI

--- test/chrome/chromeDebugAdapter.test.ts
+++ test/chrome/chromeDebugAdapter.test.ts
@@ -420,13 +420,13 @@ suite('ChromeDebugAdapter', () => {
 
         function setupEvalMock(expression: string, result: Crdp.Runtime.RemoteObject): void {
             mockChrome.Runtime
-                .setup(x => x.evaluate(It.isValue({ expression, silent: true, generatePreview: true })))
+                .setup(x => x.evaluate(It.isValue({ expression, silent: true, generatePreview: true, includeCommandLineAPI: true })))
                 .returns(() => Promise.resolve(<Crdp.Runtime.EvaluateResponse>{ result }));
         }
 
         function setupEvalOnCallFrameMock(expression: string, callFrameId: string, result: Crdp.Runtime.RemoteObject): void {
             mockChrome.Debugger
-                .setup(x => x.evaluateOnCallFrame(It.isValue({ expression, callFrameId, silent: true, generatePreview: true })))
+                .setup(x => x.evaluateOnCallFrame(It.isValue({ expression, callFrameId, silent: true, generatePreview: true, includeCommandLineAPI: true })))
                 .returns(() => Promise.resolve(<Crdp.Runtime.EvaluateResponse>{ result }));
         }
 
