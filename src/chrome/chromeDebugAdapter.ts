f01b41e0b9557509f75a17aef90a7e9636461bb4
Chance An <cha@microsoft.com>
2018-03-01 12:48:55 -0800

    Enabled telemetry measurement for scriptParsed event.

--- src/chrome/chromeDebugAdapter.ts
+++ src/chrome/chromeDebugAdapter.ts
@@ -385,7 +385,11 @@ export abstract class ChromeDebugAdapter implements IDebugAdapter {
             });
         });
         this.chrome.Debugger.onResumed(() => this.onResumed());
-        this.chrome.Debugger.onScriptParsed(params => this.onScriptParsed(params));
+        this.chrome.Debugger.onScriptParsed((params) => {
+            this.runAndMeasureProcessingTime('target/notification/onScriptParsed', () => {
+                return this.onScriptParsed(params);
+            });
+        });
         this.chrome.Debugger.onBreakpointResolved(params => this.onBreakpointResolved(params));
 
         this.chrome.Console.onMessageAdded(params => this.onMessageAdded(params));
@@ -596,7 +600,7 @@ export abstract class ChromeDebugAdapter implements IDebugAdapter {
 
         this._expectingStopReason = undefined;
 
-        smartStepP.then(should => {
+        await smartStepP.then(should => {
             if (should) {
                 this._smartStepCount++;
                 return this.stepIn(false);
