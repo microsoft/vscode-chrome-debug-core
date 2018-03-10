7fbc44ad2cd2401e07012af7016a5bcdfa38abab
Diego Geffner <digeff@microsoft.com>
2018-03-09 16:34:07 -0800

    Remove sample telemetry

--- src/executionTimingsReporter.ts
+++ src/executionTimingsReporter.ts
@@ -78,35 +78,6 @@ export class StepProgressEventsEmitter extends EventEmitter {
     }
 }
 
-/* Use to track the time executing each step during launch
-    The report/telemetry generated looks like this:
-        All	5271.022216 // Time since the reporter is created (which is basically when we launched the debug adapter itself) until we navigated to the user's web page
-        BeforeFirstStep	[7.833247] // Time since the reporter is created (which is basically when we launched the debug adapter itself) until we get the first request
-        ClientRequest.initialize	[1.261372] // Time we spent processing the initialize request
-        WaitingAfter.ClientRequest.initialize	[74.939561] // Time we spent after processing the initialize request for the client to send us another request
-        ClientRequest.launch	[2.748096] // Time we spent processing the first part of the launch request until we actually start launching the target debugee .exe
-        LaunchTarget.LaunchExe	[10.276666] // The time it takes to spawn the  the target debugee .exe (We launch the .exe but we don't wait for it)
-        Attach	[0.731042] // Time spent in general attach logic
-        Attach.RequestDebuggerTargetsInformation	[511.098151] // Time we spend requesting the targets from the debugee using the /json/list endpoint
-        Attach.ProcessDebuggerTargetsInformation	[1.270085] // Time we spend processing the HTTP response from  /json/list endpoint
-        Attach.AttachToTargetDebuggerWebsocket	[5.268137] // Time we spend attaching to the websocket
-        Attach.ConfigureDebuggingSession.Internal	[0.486761] // After we connected to the websocket, time we spent initializing our internal configuration
-        Attach.ConfigureDebuggingSession.Target	[18.861989] // After we connected to the websocket, time we spent configuring the target, enabling domains, getting schemas, etc...
-        WaitingAfter.ClientRequest.launch	[17.472918] // Time we spent after processing the launch request for the client to send us another request
-        ClientRequest.setBreakpoints	[3.708698] // Time we spent processing the set breakpoints request
-        WaitingAfter.ClientRequest.setBreakpoints	[0.343137] // Time we spent after processing the setBreakpoints request for the client to send us another request
-        ClientRequest.setExceptionBreakpoints	[0.927851] // Time we spent processing the set exception breakpoints request
-        WaitingAfter.ClientRequest.setExceptionBreakpoints	[245.659565] // Time we spent after processing the set exception breakpoints request for the client to send us another request
-        ClientRequest.configurationDone	[0.326911] // Time we spend in the configuration done request before asking the target to navigate to the user's page
-        ConfigureTarget.RequestNavigateToUserPage	[0.529427] // Time we spend requesting the debugee target to navigate to the user's page (we don't wait for it to do it, just to ACK it)
-        WaitingAfter.ClientRequest.configurationDone	[4367.064368] // Time we spend waiting for another request (This is normally the time it takes the debugee target to navigate to the user's web page, probably due to the web server taking time to answer)
-        steps	["BeforeFirstStep","ClientRequest.initialize","WaitingAfter.ClientRequest.initialize","ClientRequest.launch","LaunchTarget.LaunchExe","Attach",
-            "Attach.RequestDebuggerTargetsInformation","Attach.ProcessDebuggerTargetsInformation","Attach.AttachToTargetDebuggerWebsocket","Attach.ConfigureDebuggingSession.Internal",
-            "Attach.ConfigureDebuggingSession.Target","WaitingAfter.ClientRequest.launch","ClientRequest.setBreakpoints","WaitingAfter.ClientRequest.setBreakpoints",
-            "ClientRequest.setExceptionBreakpoints","WaitingAfter.ClientRequest.setExceptionBreakpoints","ClientRequest.configurationDone","ConfigureTarget.RequestNavigateToUserPage",
-            "WaitingAfter.ClientRequest.configurationDone"] // The order in which steps were recorder during this execution
- */
-
 class SubscriptionManager {
     private _removeSubscriptionActions = [] as [() => void];
 
