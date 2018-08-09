b147ecd07e8c4c6fe9920c6ff1ec0f3e90aee21d
digeff <digeff@hotmail.com>
2018-08-09 14:26:57 -0700

    fix_test

--- test/sourceMaps/sourceMapUtils.test.ts
+++ test/sourceMaps/sourceMapUtils.test.ts
@@ -111,7 +111,7 @@ suite('SourceMapUtils', () => {
         test('Adds ClientApp to the path in VisualStudio as a fallback to the ASP.NET Angular Template in 2.1', () => {
             mockery.resetCache();
             mockery.registerMock('fs', { statSync: (path: string) => {
-                if (path === 'c:\\project\\ClientApp\\src\\app\\counter\\counter.component.ts') {
+                if (path.toLowerCase() === testUtils.pathResolve('/project/ClientApp/src/app/counter/counter.component.ts').toLowerCase()) {
                     return true;
                 }
                 throw new Error(`File doesn't exist: ${path}`);
