ab4c1766a984725a9051b30f4860c72ae927fee2
digeff <digeff@hotmail.com>
2018-12-03 13:52:21 -0800

    Core files of the script model

---
+++ src/chrome/internal/scripts/executionContext.ts
@@ -0,0 +1,22 @@
+/** This interface represents the execution context in CDTP where a script is executed. A new context is created when a page is refreshed, etc...
+ * We keep track of this because only scripts of non destroyed execution contexts should be displayed to the user
+ */
+export interface IExecutionContext {
+    isDestroyed(): boolean;
+}
+
+export class ExecutionContext implements IExecutionContext {
+    private _isDestroyed = false;
+
+    public isDestroyed(): boolean {
+        return this._isDestroyed;
+    }
+
+    public markAsDestroyed(): void {
+        if (this._isDestroyed === false) {
+            this._isDestroyed = true;
+        } else {
+            throw new Error(`The execution context ${this} was already marked as destroyed`);
+        }
+    }
+}
---
+++ src/chrome/internal/scripts/script.ts
@@ -0,0 +1,117 @@
+import * as fs from 'fs';
+import {
+    ILoadedSource, SourceOfCompiled, ScriptRunFromLocalStorage, DynamicScript,
+    ScriptRuntimeSource, ScriptDevelopmentSource, NoURLScriptSource
+} from '../sources/loadedSource';
+import { CDTPScriptUrl } from '../sources/resourceIdentifierSubtypes';
+import { IValidatedMap } from '../../collections/validatedMap';
+import { printArray } from '../../collections/printing';
+import { ISourcesMapper } from './sourcesMapper';
+import { IResourceIdentifier, IResourceLocation, newResourceIdentifierMap, parseResourceIdentifier, ResourceName } from '../sources/resourceIdentifier';
+import { IExecutionContext } from './executionContext';
+
+/** This interface represents a piece of code that is being executed in the debugee. Usually a script matches to a file or a url, but that is not always the case.
+ * This interface solves the problem of finding the different loaded sources associated with a script, and being able to identify and compare both scripts and sources easily.
+ */
+export interface IScript {
+    readonly executionContext: IExecutionContext;
+    readonly runtimeSource: ILoadedSource<CDTPScriptUrl>; // Source in Webserver
+    readonly developmentSource: ILoadedSource; // Source in Workspace
+    readonly sourcesOfCompiled: SourceOfCompiled[]; // Sources before compilation
+    readonly allSources: ILoadedSource[]; // runtimeSource + developmentSource + sourcesOfCompiled
+    readonly url: CDTPScriptUrl;
+
+    readonly sourcesMapper: ISourcesMapper; // TODO DIEGO: See if we can delete this property
+
+    getSource(sourceIdentifier: IResourceIdentifier): ILoadedSource;
+
+    isEquivalent(source: IScript): boolean;
+}
+
+export class Script implements IScript {
+    private readonly _runtimeSource: ILoadedSource<CDTPScriptUrl>;
+    private readonly _developmentSource: ILoadedSource;
+    private readonly _compiledSources: IValidatedMap<IResourceIdentifier, SourceOfCompiled>;
+
+    public static create(executionContext: IExecutionContext, locationInRuntimeEnvironment: IResourceLocation<CDTPScriptUrl>, locationInDevelopmentEnvinronment: IResourceLocation,
+        sourcesMapper: ISourcesMapper): Script {
+        const sourcesOfCompiled = (script: IScript) => newResourceIdentifierMap<SourceOfCompiled>(sourcesMapper.sources.map(path => {
+            const identifier = parseResourceIdentifier(path);
+            return [identifier, new SourceOfCompiled(script, identifier, 'TODO DIEGO')] as [IResourceIdentifier, SourceOfCompiled];
+        }));
+
+        /**
+         * Loaded Source classification:
+         * Is the script content available on a single place, or two places? (e.g.: You can find similar scripts in multiple different paths)
+         *  1. Single: Is the single place on the user workspace, or is this a dynamic script?
+         *      Single path on storage: RuntimeScriptRunFromStorage
+         *      Single path not on storage: DynamicRuntimeScript
+         *  2. Two: We assume one path is from the webserver, and the other path is in the workspace: RuntimeScriptWithSourceOnWorkspace
+         */
+        let runtimeSource: (script: IScript) => ILoadedSource<CDTPScriptUrl>;
+        let developmentSource: (script: IScript) => ILoadedSource;
+        if (locationInRuntimeEnvironment.isEquivalent(locationInDevelopmentEnvinronment) || locationInDevelopmentEnvinronment.textRepresentation === '') {
+            if (fs.existsSync(locationInRuntimeEnvironment.textRepresentation)) {
+                developmentSource = runtimeSource = script => new ScriptRunFromLocalStorage(script, locationInRuntimeEnvironment, 'TODO DIEGO');
+            } else {
+                developmentSource = runtimeSource = script => new DynamicScript(script, locationInRuntimeEnvironment, 'TODO DIEGO');
+            }
+        } else {
+            // The script is served from one location, and it's on the workspace on a different location
+            runtimeSource = script => new ScriptRuntimeSource(script, locationInRuntimeEnvironment, 'TODO DIEGO');
+            developmentSource = script => new ScriptDevelopmentSource(script, locationInDevelopmentEnvinronment, 'TODO DIEGO');
+        }
+        return new Script(executionContext, runtimeSource, developmentSource, sourcesOfCompiled, sourcesMapper);
+    }
+
+    public static createEval(executionContext: IExecutionContext, name: ResourceName<CDTPScriptUrl>, sourcesMapper: ISourcesMapper): Script {
+        // TODO DIEGO Return the same instance both functions
+        const getNoURLScript = (script: IScript) => new NoURLScriptSource(script, name, 'TODO DIEGO');
+        return new Script(executionContext, getNoURLScript, getNoURLScript, _ => new Map<IResourceIdentifier, SourceOfCompiled>(), sourcesMapper);
+    }
+
+    constructor(public readonly executionContext: IExecutionContext, getRuntimeSource: (script: IScript) => ILoadedSource<CDTPScriptUrl>, getDevelopmentSource: (script: IScript) => ILoadedSource,
+        getCompiledScriptSources: (script: IScript) => Map<IResourceIdentifier, SourceOfCompiled>, public readonly sourcesMapper: ISourcesMapper) {
+        this._runtimeSource = getRuntimeSource(this);
+        this._developmentSource = getDevelopmentSource(this);
+        this._compiledSources = newResourceIdentifierMap(getCompiledScriptSources(this));
+    }
+
+    public get developmentSource(): ILoadedSource {
+        return this._developmentSource;
+    }
+
+    public get runtimeSource(): ILoadedSource<CDTPScriptUrl> {
+        return this._runtimeSource;
+    }
+
+    public get sourcesOfCompiled(): SourceOfCompiled[] {
+        return Array.from(this._compiledSources.values());
+    }
+
+    public getSource(sourceIdentifier: IResourceIdentifier): ILoadedSource {
+        return this._compiledSources.get(sourceIdentifier);
+    }
+
+    public get allSources(): ILoadedSource[] {
+        const unmappedSources: ILoadedSource[] = [this.runtimeSource];
+        if (this.developmentSource !== this.runtimeSource) {
+            unmappedSources.push(this.developmentSource);
+        }
+
+        return unmappedSources.concat(this.sourcesOfCompiled);
+    }
+
+    public get url(): CDTPScriptUrl {
+        return this._runtimeSource.identifier.textRepresentation;
+    }
+
+    public isEquivalent(script: Script): boolean {
+        return this === script;
+    }
+
+    public toString(): string {
+        return `Script:\n  Runtime source: ${this.runtimeSource}\n  Development source: ${this.developmentSource}\n`
+            + printArray('  Sources of compiledsource', this.sourcesOfCompiled);
+    }
+}
