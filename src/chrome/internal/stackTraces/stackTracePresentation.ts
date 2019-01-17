2954ad7d102ca29be9e7b5fe7194f0e425d6f3e1
D <digeff@microsoft.com>
2019-01-16 18:27:43 -0800

    Fixed comment about StackTraceLabel

--- src/chrome/internal/stackTraces/stackTracePresentation.ts
+++ src/chrome/internal/stackTraces/stackTracePresentation.ts
@@ -7,7 +7,7 @@
 /** The stack traces we sent to the client will be represented by this classes and it is a combination of:
  *    1. CallFrames with state information from the sync frames.
  *    2. CodeFlowFrames without state information from async frames.
- *    3. Labels that we use to [Show more frames] or [Frames skipped by smartStep], etc...
+ *    3. Labels that we use to separate and show the description of the async frames
  */
  export interface StackTracePresentation {
     readonly stackFrames: IStackTracePresentationRow[];
