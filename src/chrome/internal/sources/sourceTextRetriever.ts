35cfa93dfd69bbb96dd76d3adbffc5d8f48a9ceb
D <digeff@microsoft.com>
2019-07-01 10:07:23 -0700

    Now we don't fail if we get 2 simultaneos requests

--- src/chrome/internal/sources/sourceTextRetriever.ts
+++ src/chrome/internal/sources/sourceTextRetriever.ts
@@ -19,17 +19,18 @@ import { printArray } from '../../collections/printing';
  */
 @injectable()
 export class SourceTextRetriever {
-    private _sourceToText = new ValidatedMap<ILoadedSource, string>();
+    private _sourceToText = new ValidatedMap<ILoadedSource, Promise<string>>();
 
     constructor(@inject(TYPES.IScriptSources) private readonly _scriptSources: IScriptSourcesRetriever) { }
 
-    public async text(loadedSource: ILoadedSource): Promise<string> {
+    // We want this method to be sync, so if we get 2 simultaneous calls, the second call will return the promise of the first call
+    public text(loadedSource: ILoadedSource): Promise<string> {
         let text = this._sourceToText.tryGetting(loadedSource);
 
         if (text === undefined) {
             const scripts = loadedSource.scriptMapper().scripts;
             if (loadedSource.sourceScriptRelationship === SourceScriptRelationship.SourceIsSingleScript && scripts.length === 1) {
-                text = await this._scriptSources.getScriptSource(singleElementOfArray(scripts));
+                text = this._scriptSources.getScriptSource(singleElementOfArray(scripts));
             } else if (loadedSource.sourceScriptRelationship === SourceScriptRelationship.SourceIsSingleScript && scripts.length >= 2) {
                 /**
                  * We have two scripts associated with this source. At the moment we don't have any further support for this scenario
@@ -37,10 +38,10 @@ export class SourceTextRetriever {
                  * isn't the same.
                  */
                 logger.warn(`${loadedSource} is associated with several ${printArray('scripts', scripts)} returning the source arbitrarily of the first one`);
-                text = await this._scriptSources.getScriptSource(scripts[0]);
+                text = this._scriptSources.getScriptSource(scripts[0]);
             } else if (loadedSource.contentsLocation === ContentsLocation.PersistentStorage) {
                 // If this is a file, we don't want to cache it, so we return the contents immediately
-                return await utils.readFileP(loadedSource.identifier.textRepresentation);
+                return utils.readFileP(loadedSource.identifier.textRepresentation);
             } else {
                 // We'll need to figure out what is the right thing to do for SourceScriptRelationship.Unknown
                 throw new Error(`Support for getting the text from dynamic sources that have multiple scripts embedded hasn't been implemented yet`);
